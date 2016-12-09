d467b25db8faa38c17be5270322a9f916f5800fd
Shaky <shaky153@gmail.com>
2016-12-09 12:10:36 +0000

    Mi5 Nougat Kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#6 SMP PREEMPT Thu Dec 8 14:44:11 GMT 2016"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "shaky156-MS-7816"
+#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (Linaro GCC 4.9-2015.06) "
